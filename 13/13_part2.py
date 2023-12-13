7af58756ef1a9857df0aa8dcba3681796193695f
georgescutelnicu <george.v.scutelnicu@gmail.com>
2023-12-13 22:36:57 +0200

    Day 13 part2

---
+++ 13/13_part2.py
@@ -0,0 +1,33 @@
+def search(patterns, multiply=True):
+  res = 0
+  
+  for pattern in patterns:
+    for i in range(1, len(pattern)):
+      first, second = pattern[:i], pattern[i:]
+      l = len(first) if len(first) < len(second) else len(second)
+      first, second = list(reversed(first))[:l], second[:l]
+
+      # Check if there is only one difference between first and second
+      first_elements = [f_ for f in first for f_ in f]
+      second_elements = [s_ for s in second for s_ in s]
+      diff = sum(f != s for f, s in zip(first_elements, second_elements))
+
+      if diff == 1:
+        if multiply:
+          res +=  i * 100
+        else:
+          res += i
+
+  return res
+
+
+with open('13.txt', 'r') as f:
+  patterns = [pattern.splitlines() for pattern in f.read().split("\n\n")]
+  transposed_patterns = [[''.join(row) for row in zip(*pattern)] for pattern in patterns]
+
+  res = 0
+
+  res += search(patterns)
+  res += search(transposed_patterns, multiply=False)
+            
+  print(res)
